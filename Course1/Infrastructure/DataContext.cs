363a41232b63ac5fd938c7867f11ffd24037ddcb
koszatree <koszatree@gmail.com>
2023-10-24 17:12:40 +0200

    24.Oct - DbContext Class

---
+++ Course1/Infrastructure/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Course1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Course1.Infrastructure
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
--- Course1/Program.cs
+++ Course1/Program.cs
@@ -4,89 +4,25 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDistributedMemoryCache();
-
-builder.Services.AddSession(options =>
+builder.Services.AddHsts(options =>
 {
-    options.IdleTimeout = TimeSpan.FromMinutes(30);
-    options.Cookie.IsEssential = true;
+    options.MaxAge = TimeSpan.FromDays(1);
+    options.IncludeSubDomains = true;
 });
-
-builder.Services.Configure<FruitOptions>(options =>
-{
-    options.Name = "watermelon";
-});
-
-builder.Services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
-builder.Services.AddScoped<IResponseFormatter, GuidService>();
 
 var app = builder.Build();
 
-app.UseSession();
-
-app.MapGet("/formatter1", async (HttpContext context, IResponseFormatter formatter) =>
+app.MapGet("/https", async context =>
 {
-    await formatter.Format(context, "Formatter 1");
+    await context.Response.WriteAsync($"HTTPS Request: {context.Request.IsHttps}");
 });
 
-app.MapGet("/formatter2", async (HttpContext context, IResponseFormatter formatter) =>
+app.UseHttpsRedirection();
+
+if (app.Environment.IsProduction())
 {
-    await formatter.Format(context, "Formatter 2");
-});
-
-app.UseMiddleware<CustomMiddleware>();
-
-app.MapGet("/endpoint", CustomEndpoint.Endpoint);
-
-app.UseMiddleware<FruitMiddleware>();
-
-app.MapGet("/config", async (HttpContext context, IConfiguration config) =>
-{
-    string defaultDebug = config["Logging:LogLevel:Default"];
-    await context.Response.WriteAsync(defaultDebug);
-
-    string environment = config["ASPNETCORE_ENVIRONMENT"];
-    await context.Response.WriteAsync(environment);
-
-    if (app.Environment.IsDevelopment())
-    {
-        await context.Response.WriteAsync("\nisDevelopment");
-    }
-});
-
-app.MapGet("/cookie", async context =>
-{
-    int counter = int.Parse(context.Request.Cookies["counter"] ?? "0") + 1;
-
-    context.Response.Cookies.Append(
-            "counter",
-            counter.ToString(),
-            new CookieOptions
-            {
-                MaxAge = TimeSpan.FromMinutes(30)
-            }
-        );
-
-    await context.Response.WriteAsync($"Cookie: {counter}");
-});
-
-app.MapGet("/clear", context =>
-{
-    context.Response.Cookies.Delete("counter");
-    context.Response.Redirect("/");
-    return Task.CompletedTask;
-});
-
-app.MapGet("/session", async context =>
-{
-    int sessionCounter = (context.Session.GetInt32("counter") ?? 0) + 1;
-
-    context.Session.SetInt32("counter", sessionCounter);
-
-    await context.Session.CommitAsync();
-
-    await context.Response.WriteAsync($"Session: {sessionCounter}");
-});
+    app.UseHsts();
+}
 
 app.MapGet("/", () => "Hello World!");
 
