de36c3e2ec55c020b7227359446bc2eb4bf87a57
koszatree <koszatree@gmail.com>
2023-10-24 15:30:49 +0200

    24/Oct

--- Course1/Program.cs
+++ Course1/Program.cs
@@ -4,22 +4,25 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.IsEssential = true;
+});
+
 builder.Services.Configure<FruitOptions>(options =>
 {
     options.Name = "watermelon";
 });
 
 builder.Services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
-builder.Services.AddTransient<IResponseFormatter, GuidService>();
+builder.Services.AddScoped<IResponseFormatter, GuidService>();
 
 var app = builder.Build();
 
-//app.MapGet("/fruit", async(HttpContext context, IOptions<FruitOptions> FruitOptions) => {
-//    FruitOptions options = FruitOptions.Value;
-//    await context.Response.WriteAsync($"{options.Name}, {options.Color}");
-//});
-
-//IResponseFormatter formatter = new TextResponseFormatter();
+app.UseSession();
 
 app.MapGet("/formatter1", async (HttpContext context, IResponseFormatter formatter) =>
 {
@@ -32,11 +35,58 @@ app.MapGet("/formatter2", async (HttpContext context, IResponseFormatter formatt
 });
 
 app.UseMiddleware<CustomMiddleware>();
-app.UseMiddleware<CustomMiddleware2>();
 
 app.MapGet("/endpoint", CustomEndpoint.Endpoint);
 
 app.UseMiddleware<FruitMiddleware>();
+
+app.MapGet("/config", async (HttpContext context, IConfiguration config) =>
+{
+    string defaultDebug = config["Logging:LogLevel:Default"];
+    await context.Response.WriteAsync(defaultDebug);
+
+    string environment = config["ASPNETCORE_ENVIRONMENT"];
+    await context.Response.WriteAsync(environment);
+
+    if (app.Environment.IsDevelopment())
+    {
+        await context.Response.WriteAsync("\nisDevelopment");
+    }
+});
+
+app.MapGet("/cookie", async context =>
+{
+    int counter = int.Parse(context.Request.Cookies["counter"] ?? "0") + 1;
+
+    context.Response.Cookies.Append(
+            "counter",
+            counter.ToString(),
+            new CookieOptions
+            {
+                MaxAge = TimeSpan.FromMinutes(30)
+            }
+        );
+
+    await context.Response.WriteAsync($"Cookie: {counter}");
+});
+
+app.MapGet("/clear", context =>
+{
+    context.Response.Cookies.Delete("counter");
+    context.Response.Redirect("/");
+    return Task.CompletedTask;
+});
+
+app.MapGet("/session", async context =>
+{
+    int sessionCounter = (context.Session.GetInt32("counter") ?? 0) + 1;
+
+    context.Session.SetInt32("counter", sessionCounter);
+
+    await context.Session.CommitAsync();
+
+    await context.Response.WriteAsync($"Session: {sessionCounter}");
+});
 
 app.MapGet("/", () => "Hello World!");
 
